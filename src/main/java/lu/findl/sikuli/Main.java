819f5a364b38c72b332e93309e014861254f544e
RAFIKI Houssam <houssam-eddine.rafiki@findl.lu>
2019-08-08 15:28:08 +0100

    until add BOs works fine

--- src/main/java/lu/findl/sikuli/Main.java
+++ src/main/java/lu/findl/sikuli/Main.java
@@ -4,59 +4,26 @@ import org.sikuli.basics.Debug;
 import org.sikuli.script.*;
 
 public class Main {
+    private static final int DEFAULT_TIMEOUT = 60; //1 minute
 
     public static void main(String[] args) throws FindFailed {
 
-        Debug.setDebugLevel(3);
-        ImagePath.add("src/main/resources");
-        Screen sc = new Screen(2);
+        Debug.setDebugLevel(2);
+        ImagePath.add(Main.class.getCanonicalName() + "/imgs");
+        Screen sc = new Screen();
 
         String[] data = {"Houssam", "Imane", "Othmane"};
 
-        App.open("AcroRd32",5); // open Adobe acrobat reader
-        sc.doubleClick("imgs/form2.png");// open form2
-        sc.wait(6.0);
+        App.focus("AcroRd32");// open acrobat reader
+        App pdf = App.open("\"c:\\RBE\\form_1.pdf\""); // open form 1
+        sc.wait(new Pattern("logo.png").exact(), DEFAULT_TIMEOUT);// wait till pdf load
 
-        Match zoomArriere = sc.find("imgs/zoomArriere.png"); // resize adobe acrobat screen size to make page switch easy
-        for (int i = 0; i < 4; i++)
-            sc.click(zoomArriere);
+        sc.type(Key.RIGHT, KeyModifier.CTRL); // go add bo page
 
-        sc.click("imgs/nextPage.png"); // go to add one or more bo page
-
-        for (int i = 0; i < data.length -1; i++) { // Add bo
-            sc.click("imgs/addBoButton.png");
-            sc.wait(1.0);
+        for (int i = 0; i < data.length - 1; i++) { // Add bos
+            sc.click(sc.wait(new Pattern("addBoButton").exact(), DEFAULT_TIMEOUT));
+            sc.mouseMove(sc.getCenter());
         }
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
